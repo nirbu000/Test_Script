4830ca8091cf479509b54d23f624c6af6b5f769f
nirbu000 <nirbu000@sharklasers.com>
2017-06-07 18:03:48 +0300

    added the script file

---
+++ TestExecutable.py
@@ -0,0 +1,22 @@
+import subprocess
+import requests
+
+
+url = 'http://is-hw.getsandbox.com/test/'
+header = {"Content-Type": "text/plain", "API-Key": "api-6cf4ee44-6284-418c-b4f4-8284869d6588"}
+
+
+def is_vs_av():
+    exename = 'iS.exe'
+    for i in range(1, 11):
+        avName = 'AV{0}.exe'.format(i)
+        result = subprocess.call(['C:\AV\/' + avName, 'C:\Program Files (x86)\Jenkins\workspace\Dev\bin\Debug\/' + exename])
+        if result == 0:
+            response = requests.put(url + avName + '/' + exename, data="Test with {0}".format(avName), headers=header, params={'flagged': 'false'})
+            print(response)
+        else:
+            response = requests.put(url + avName + '/' + exename, data="Test with {0}".format(avName), headers=header, params={'flagged': 'true'})
+            print(response)
+
+
+is_vs_av()
